c1caa04103386e38c75bbf7a13f4a7e7f33f20cb
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-07-31 17:29:29 -0300

    delete PhoneViewset

--- desafio/core/views.py
+++ desafio/core/views.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework import generics
-from desafio.core.serializers import UserSerializer, PhoneSerializer
+from desafio.core.models import User
+from rest_framework import viewsets, permissions, generics
+from desafio.core.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -12,14 +10,6 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-
-class PhoneViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows phones to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = PhoneSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class SignupView(generics.CreateAPIView):
     """
