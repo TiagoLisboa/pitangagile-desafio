8a86396c3fe5be219991cccac5ff7ba48542deec
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-07-31 18:56:25 -0300

    support creating multiple phones withing UserSerializer

--- desafio/core/serializers.py
+++ desafio/core/serializers.py
@@ -18,6 +18,13 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ['created_at', 'last_login']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        phones_data = validated_data.pop('phones')
+        user = User.objects.create(**validated_data)
+        phones = [Phone(user=user, **phone) for phone in phones_data]
+        phones = Phone.objects.bulk_create(phones)
+        return user
+
 
 class SigninSerializer(TokenObtainPairSerializer):
     @classmethod
