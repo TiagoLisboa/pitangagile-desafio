c4e3806c412c6f2df8431c3dbd039d09a4828712
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-07-31 13:27:35 -0300

    create UserViewSet and PhoneViewSet

--- desafio/core/views.py
+++ desafio/core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from desafio.core.serializers import UserSerializer, PhoneSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PhoneViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows phones to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = PhoneSerializer
+    permission_classes = [permissions.IsAuthenticated]
