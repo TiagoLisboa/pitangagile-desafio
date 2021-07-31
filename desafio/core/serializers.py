78339b0e7d6382b9ed1764790731532633eb3d20
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-07-31 18:04:11 -0300

    fix phones serialization

--- desafio/core/serializers.py
+++ desafio/core/serializers.py
@@ -1,15 +1,18 @@
 from .models import Phone, User
 from rest_framework import serializers
 
+
+class PhoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phone
+        fields = ['number', 'area_code', 'country_code']
+
+
 class UserSerializer(serializers.ModelSerializer):
+    phones = PhoneSerializer(many=True)
     class Meta:
         model = User
         fields = ['firstName', 'lastName', 'email', 'phones', 'created_at', 'last_login', 'password']
         read_only_fields = ['created_at', 'last_login']
         extra_kwargs = {'password': {'write_only': True}}
 
-
-class PhoneSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Phone
-        fields = ['number', 'area_code', 'country_code']
