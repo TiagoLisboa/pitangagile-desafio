be4ae5d993b6b6b8ff4bd7c486a1e9f4836abc91
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-07-31 12:28:09 -0300

    rename users app to core

--- desafio/core/models.py
+++ desafio/core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 
-from desafio.users.managers import UserManager
+from desafio.core.managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
