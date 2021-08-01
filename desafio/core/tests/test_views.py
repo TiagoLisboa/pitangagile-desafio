eeed89415f53b5309eed9a66a5cc48370340ac88
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-08-01 17:16:12 -0300

    create UserViewTest@test_handle_exception_not_authenticated

--- desafio/core/tests/test_views.py
+++ desafio/core/tests/test_views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase, RequestFactory
+from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
+from rest_framework import status
 
 from ..views import UserView
+from ..exceptions import EmailAlreadyExistsException, InvalidFieldsException, MissingFieldsException
 
 class UserViewTest(TestCase):
-    """ Test module for User Model """
+    """ Test module for UserView """
 
     def setUp(self):
         pass
@@ -17,3 +20,16 @@ class UserViewTest(TestCase):
         view = UserView(request=req)
 
         self.assertEquals(view.get_object(), user)
+
+    def test_handle_exception_not_authenticated(self):
+        view = UserView()
+        response = view.handle_exception(NotAuthenticated())
+
+        self.assertEquals(response.data, {
+            'message': 'Unauthorized',
+            'errorCode': status.HTTP_401_UNAUTHORIZED,
+        })
+        self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+
