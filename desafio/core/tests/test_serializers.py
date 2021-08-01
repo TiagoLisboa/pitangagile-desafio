5133d1faadd1f513241fe2f69cbd29927a99325b
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-08-01 16:27:40 -0300

    create test_signin_serializer_validate

--- desafio/core/tests/test_serializers.py
+++ desafio/core/tests/test_serializers.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from unittest import mock
 
 from ..serializers import UserSerializer, SigninSerializer
 from ..models import Phone, User
@@ -39,10 +40,31 @@ class SigninSerializerTest(TestCase):
     """ Test Module for SigninSerializer """
 
     def setUp(self) -> None:
-        self.user = User.objects.create(firstName="John", lastName="Doe", email="johndoe@email.com", password="secret")
+        data = {
+            'firstName': 'John',
+            'lastName': 'Doe',
+            'email': 'johndoe@email.com',
+            'password': 'secret',
+            'phones': []
+        }
+        serializer = UserSerializer(data=data)
+        serializer.is_valid()
+        serializer.save()
+        self.user = serializer.instance
         return super().setUp()
 
     def test_signin_serializer_get_token(self):
         serializer = SigninSerializer(self.user)
         token = serializer.get_token(self.user)
         self.assertEquals(token['email'], self.user.email)
+
+    def test_signin_serializer_validate(self):
+        serializer = SigninSerializer(data={
+            'email': 'johndoe@email.com',
+            'password': 'secret'
+        })
+        with mock.patch('desafio.core.serializers.update_last_login') as mock_update_last_login:
+            serializer.is_valid()
+
+        mock_update_last_login.assert_called()
+
