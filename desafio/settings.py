c8485e922574f8c0f5272f02f874c8d7d481b9e2
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-08-01 19:13:21 -0300

    configure django_heroku in the end of settings.py

--- desafio/settings.py
+++ desafio/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Configure django app for heroku
-import django_heroku
-django_heroku.settings(locals())
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +136,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure django app for heroku
+import django_heroku
+django_heroku.settings(locals())
+
