284bdbb87789b3b58f12774b753a80371cbbfad1
Tiago Lisboa <tiago.lisboa@devsquad.com>
2021-08-01 11:44:05 -0300

    use inheritance on exceptions

--- desafio/core/exceptions.py
+++ desafio/core/exceptions.py
@@ -1,52 +1,29 @@
 from rest_framework.exceptions import PermissionDenied
 from rest_framework import status
 
-class InvalidFieldsException(PermissionDenied):
+class CustomApiExceptions(PermissionDenied):
     status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = {
-        'message': 'Invalid Fields',
-        'errorCode': 400,
-    }
+    message = 'Exception'
     default_code = 'invalid'
 
     def __init__(self, message=None, status_code=None):
         if message is not None:
-            self.default_detail['message'] = message
+            self.message = message
         if status_code is not None:
             self.status_code = status_code
-            self.default_detail['errorCode'] = status_code
-        self.detail = self.default_detail
+        self.detail = {
+            'message': self.message,
+            'errorCode': self.status_code
+        }
 
 
-class MissingFieldsException(PermissionDenied):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = {
-        'message': 'Missing Fields',
-        'errorCode': 400,
-    }
-    default_code = 'invalid'
-
-    def __init__(self, message=None, status_code=None):
-        if message is not None:
-            self.default_detail['message'] = message
-        if status_code is not None:
-            self.status_code = status_code
-            self.default_detail['errorCode'] = status_code
-        self.detail = self.default_detail
+class InvalidFieldsException(CustomApiExceptions):
+    message = 'Invalid Fields'
 
 
-class EmailAlreadyExistsException(PermissionDenied):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = {
-        'message': 'E-mail already exists',
-        'errorCode': 400,
-    }
-    default_code = 'invalid'
+class MissingFieldsException(CustomApiExceptions):
+    message = 'Missing Fields'
 
-    def __init__(self, message=None, status_code=None):
-        if message is not None:
-            self.default_detail['message'] = message
-        if status_code is not None:
-            self.status_code = status_code
-            self.default_detail['errorCode'] = status_code
-        self.detail = self.default_detail
+
+class EmailAlreadyExistsException(CustomApiExceptions):
+    message = 'E-mail already exists'
